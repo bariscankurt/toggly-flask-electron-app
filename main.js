9705bd92dfd36932bee353805a59dce6074a35e9
Baris Can Kurt <bariscankurt0@gmail.com>
2021-01-29 16:11:48 +0300

    version 2 update

---
+++ main.js
@@ -0,0 +1,23 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow () {
+    window = new BrowserWindow({width: 800, height: 600})
+    window.loadFile('index.html')
+    var pyshell =  require('python-shell');
+pyshell.run('engine.py',  function  (err, results)  {
+ if  (err)  console.log(err);
+});   	
+    
+}
+
+
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+})
+
+
